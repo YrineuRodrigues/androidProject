11fbd4d69208afea95f68b4ff5d0e5dabc1b6cb6
YrineuRodrigues <yfrfelipe@hotmail.com>
2013-10-24 23:29:22 -0300

    Second Commit

--- AgilePMobile/src/br/com/mobile/scrum/activity/ProjectsActivity.java
+++ AgilePMobile/src/br/com/mobile/scrum/activity/ProjectsActivity.java
@@ -4,31 +4,45 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import br.com.mobile.scrum.domain.Project;
 
-public class ProjectsActivity extends Activity{
+public class ProjectsActivity extends ListActivity {
 
-	private SimpleCursorAdapter adapter;
-	
+	private ArrayAdapter<String> adapter;
+	private Object myActionMode;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		List<Project> listProject = getListProjects();
-		final String[] projectsRow = new String[]{};
-		for(int i=0; i<listProject.size(); i++){
-			projectsRow[i]= listProject.get(i).getDescription();
+		final List<String> projectsRow = new ArrayList<String>();
+		for (int i = 0; i < listProject.size(); i++) {
+			projectsRow.add(listProject.get(i).getDescription());
 		}
-				
+
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_expandable_list_item_1,
+				(String[]) listProject.toArray());
+
+		setListAdapter(adapter);
 		
 	}
 	
-	private List<Project> getListProjects(){
-		//Código que captura os projetos do servidor
-		
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	private List<Project> getListProjects() {
+		// Código que captura os projetos do servidor
+
 		List<Project> projectList = new ArrayList<Project>();
 		projectList.add(new Project("Projeto 01", Calendar.getInstance()));
 		projectList.add(new Project("Projeto 02", Calendar.getInstance()));
